0789bbf5133165cfa4a7cf78903d44f7642b1500
nehahirve <neha.hirve@hyperisland.se>
2021-06-08 10:22:02 +0200

    fix routing

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import { useState } from "react";
 import Intro from "./Intro";
 import Scene1 from "./Scene1";
 import Scene2 from "./Scene2";
 import Scene3 from "./Scene3";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-// import Ascenetest from "./components/Ascenetest";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 const Game = () => {
 	return (
-		<div>
-			<BrowserRouter basename={process.env.PUBLIC_URL}>
-				<div className="App">
-					<Route exact path="/" component={Intro} />
-					<Route path="/scene1" component={Scene1} />
-					<Route path="/scene2" component={Scene2} />
-					<Route path="/scene3" component={Scene3} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-};
-
-export default Game;
+	  <div>
+		<Router basename='/'>
+		  <div className='App'>
+			<Switch>
+			  <Route exact path='/'>
+				<Intro />
+			  </Route>
+			  <Route path='/scene1'>
+				<Scene1 />
+			  </Route>
+			  <Route path='/scene2'>
+				<Scene2 />
+			  </Route>
+			  <Route path='/scene3'>
+				<Scene3 />
+			  </Route>
+			</Switch>
+		  </div>
+		</Router>
+	  </div>
+	)
+  }
+  
+  export default Game
